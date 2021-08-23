612a22f5d408556917fc63c870287f18c95a1543
chen206 <chen206@gmail.com>
2021-08-23 17:47:34 +0800

    add access token

--- src/main/java/com/liangjianghu/asa/ASAToken.java
+++ src/main/java/com/liangjianghu/asa/ASAToken.java
@@ -1,12 +1,22 @@
 package com.liangjianghu.asa;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
-
 import java.security.interfaces.ECKey;
 
 import java.util.Date;
 import java.util.HashMap;
+
+import java.io.Reader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+import org.apache.commons.io.IOUtils;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+import com.alibaba.fastjson.JSONObject;
 
 /**
  * apple ads java demo
@@ -25,9 +35,34 @@ public class ASAToken {
     public static void main(String[] args) {
         try {
             String clientSecret = createClientSecret(PRIVATE_KEY_FILE_256);
-            System.err.println(clientSecret);
+            System.out.println("clientSecret 建议保存，有效期最长 180 天");
+            System.out.println(clientSecret);
+
+            String url            = "https://appleid.apple.com/auth/oauth2/token";
+            String urlParameters  = "grant_type=client_credentials&scope=searchadsorg&client_id=" + client_id + "&client_secret=" + clientSecret;
+            byte[] postData       = urlParameters.getBytes( StandardCharsets.UTF_8 );
+            int    postDataLength = postData.length;
+
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            con.setDoOutput( true );
+            con.setRequestMethod( "POST" );
+            con.setRequestProperty( "charset", "utf-8" );
+            con.setRequestProperty( "Content-Length", Integer.toString( postDataLength ));
+            con.setRequestProperty( "Host", "appleid.apple.com" );
+            con.setRequestProperty( "Content-Type", "application/x-www-form-urlencoded" );
+            con.setUseCaches( false );
+            try( DataOutputStream wr = new DataOutputStream( con.getOutputStream())) {
+               wr.write( postData );
+            }
+            if (con.getResponseCode() == 200) {
+                String result = IOUtils.toString(con.getInputStream(), StandardCharsets.UTF_8);
+                JSONObject jsonObject = JSONObject.parseObject(result);
+                System.out.println("access_token 有效期1个小时");
+                System.out.println(jsonObject.getString("access_token"));
+            }
         } catch (Exception e) {
-            System.out.println("createClientSecret error");
+            System.err.println("error");
         }
     }
     /**
@@ -40,6 +75,7 @@ public class ASAToken {
         Algorithm algorithm = Algorithm.ECDSA256((ECKey) PemUtils.readPrivateKeyFromFile(PRIVATE_KEY_FILE_256, "EC"));
         return JWT.create()
                 .withIssuer(team_id)
+                .withAudience(aud)
                 .withHeader(new HashMap() {{
                     put("alg",alg);
                     put("kid",key_id);
